57bf990cc4094568dd78f8c8584a8743067dbdb3
Caodinhnhuy <caodinhnhuy@gmail.com>
2025-06-04 20:33:17 +0700

    Section4

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,32 +1,20 @@
-using System.Diagnostics;
-using CoffeeShop1.Models;
+using coffeeshop.Models.interfaces;
 using Microsoft.AspNetCore.Mvc;
 
-namespace CoffeeShop1.Controllers
+namespace coffeeshop.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private IProductRepository productRepository;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IProductRepository productRepository)
         {
-            _logger = logger;
+            this.productRepository = productRepository;
         }
 
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(productRepository.GetTrendingProducts());
         }
     }
 }
